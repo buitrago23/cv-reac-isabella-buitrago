d814c42d0971d181bf1a79a8c8ccf9197c9c6033
isabella <gallegobuitragoisa@gmail.com>
2025-05-28 14:43:00 -0500

    voy a subir este proceso al repositorio

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,8 +1,8 @@
 export default function CabeceraCV() {
   return (
     <header>
-      <h1>Juan Esteban Garcia Arboleda</h1>
-      <h2>Desarrollador Full Stack</h2>
+      <h1>Isabella Buitrago Gallego </h1>
+      <h2>Desarrolladora Full Stack y cloud enginner</h2>
     </header>
   );
 }
--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -4,13 +4,13 @@ export default function Experiencia() {
       <h3>Experiencia Profesional</h3>
       <ul>
         <li>
-          <strong>Desarrollador Full Stack</strong> - Navisoft Gaming (2022-2024)
+          <strong>Desarrollador Full Stack</strong> - GLOBANT (2020-2022)
         </li>
         <li>
-          <strong>Instructor TIC</strong> - SENA (2020-2022)
+          <strong>Desarrollador Full Stack </strong> - EPAM (2022-2024)
         </li>
         <li>
-          <strong>Logistico Merkahorro S.A.S</strong> - Supermercados Merkahorro (2024-2025)
+          <strong>Emprendedora </strong> - Aurum Decoraciones (2023-2024)
         </li>
       </ul>
     </section>

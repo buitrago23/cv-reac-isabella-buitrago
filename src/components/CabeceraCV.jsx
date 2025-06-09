ca49a4bcb756389d35a13fe1e774c47f0bffff15
buitrago23 <gallegobuitragoisa@gmail.com>
2025-06-08 23:50:06 -0500

    feat: componente CabeceraCV con datos personales actualizados

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -6,6 +6,14 @@ export default function CabeceraCV() {
     <header className="cabeza">
       <h1>Isabella Buitrago Gallego </h1>
       <h2>Desarrolladora Full Stack y cloud enginner</h2>
+      
+      <div className="contacto">
+        <p><strong>Email:</strong> isabella.buitrago@gmail.com</p>
+        <p><strong>Teléfono:</strong> +57 3193817455</p>
+        <p><strong>Ciudad:</strong> Bello</p>
+        <p><strong>Barrio</strong> Niquia</p>
+      </div>
+
     </header>
   );
 }

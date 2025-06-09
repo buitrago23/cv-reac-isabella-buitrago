18480f30ace1ffa3ad4809c34f768985888f26e4
buitrago23 <gallegobuitragoisa@gmail.com>
2025-06-08 23:17:58 -0500

    feat: CabeceraCV, dinamico.

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,6 +1,9 @@
+import "./cabecera.css";
+
+
 export default function CabeceraCV() {
   return (
-    <header>
+    <header className="cabeza">
       <h1>Isabella Buitrago Gallego </h1>
       <h2>Desarrolladora Full Stack y cloud enginner</h2>
     </header>

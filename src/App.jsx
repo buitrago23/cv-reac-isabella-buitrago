9b0b9e9f9a9245923e505f3d35bc60e1c28113d5
buitrago23 <gallegobuitragoisa@gmail.com>
2025-06-16 16:02:56 -0500

    importe los nuevos componente en el app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
-import React from "react";
-import CabeceraCV from "./components/CabeceraCV.jsx";
-import Perfil from "./components/Perfil.jsx";
-import Educacion from "./components/Educacion.jsx";
-import Experiencia from "./components/Experiencia.jsx";
-import StackTecnologias from "./components/StackTecnologias.jsx";
-import Proyectos from "./components/Proyectos.jsx";
-import Habilidades from "./components/Habilidades.jsx";
-import { cabecera, perfil, educacion, experiencia, stackTecnologias, proyectos, habilidades } from "./data/cvData";
+// src/App.jsx
+import React, { useState } from "react";
+import {
+  cabecera,
+  perfil,
+  educacion,
+  experiencia,
+  stackTecnologias as tecnologiasIniciales,
+  proyectos,
+  habilidades,
+} from "./data/cvData";
+
+import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
+import Educacion from "./components/Educacion";
+import Experiencia from "./components/Experiencia";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/Proyectos";
+import ToggleHabilidades from "./components/ToggleHabilidades";
+import FormularioTecnologia from "./components/FormularioTecnologia";
 
 function App() {
+  const [tecnologias, setTecnologias] = useState(tecnologiasIniciales);
+
+  const agregarTecnologia = (nueva) => {
+    setTecnologias((prev) => [...prev, nueva]);
+  };
+
   return (
-    <div className="app-container">
-      <CabeceraCV nombre={cabecera.nombre} profesion={cabecera.profesion} />
-      <Perfil texto={perfil.texto} />
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      <CabeceraCV {...cabecera} />
+      <Perfil {...perfil} />
       <Educacion estudios={educacion} />
-      <Experiencia experiencia={experiencia} />
-      <StackTecnologias tecnologias={stackTecnologias} />
+      <Experiencia trabajos={experiencia} />
+      <StackTecnologias tecnologias={tecnologias} />
+
+      {/* Componente con evento + estado */}
+      <FormularioTecnologia onAgregar={agregarTecnologia} />
+
+      {/* Componente con renderizado condicional */}
+      <ToggleHabilidades habilidades={habilidades} />
+
       <Proyectos proyectos={proyectos} />
-      <Habilidades habilidades={habilidades} />
     </div>
   );
 }
